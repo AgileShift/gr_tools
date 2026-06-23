2505ad92d7e23cedc204ef0160dd5d978898c885
Jeans K. Real <jkrb7@hotmail.com>
2026-06-23 14:33:00 -0600

    Revert http3 fix on socket.io

--- gr_tools/public/js/gr_tools.bundle.js
+++ gr_tools/public/js/gr_tools.bundle.js
@@ -1,16 +1,2 @@
 import './controls/multicheck_single';
 import './utils/payment_entry_quick_entry';
-
-if (frappe.realtime && !frappe.realtime.socket && !frappe.boot.disable_async) {
-	frappe.realtime.init(undefined, true);
-
-	const socket = frappe.realtime.socket;
-	socket.io.opts.transports = ['websocket', 'polling'];
-	socket.on('connect_error', () => {
-		if (!socket.io.engine?.id && socket.io.opts.transports[0] === 'websocket') {
-			socket.io.opts.transports = ['polling', 'websocket'];
-		}
-	});
-
-	frappe.realtime.connect();
-}
